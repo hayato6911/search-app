cfbda3a87ab491a46261515cf52ce8031fffdbc6
hayato6911 <hayato6911@yahoo.co.jp>
2021-10-09 17:56:05 +0900

    carrierwave設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,19 @@
-#require 'carrierwave/storage/abstract'
-#require 'carrierwave/storage/file'
-#require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-#CarrierWave.configure do |config|
-  #config.storage :fog
-  #config.fog_provider = 'fog/aws'
-  #config.fog_directory  = 'AWSで作成したバケット名'
-  #config.fog_credentials = {
-    #provider: 'AWS',
-    #aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    #aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    #region: ENV['AWS_DEFAULT_REGION'],
-    #path_style: true
-  #}
+if Rails.env.production?
+CarrierWave.configure do |config|
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
 
-#end
+    provider: 'AWS',
+    region:  ENV['S3_REGION'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+
+  }
+  config.fog_directory  = ENV['S3_BUCKET']
+end
+end
