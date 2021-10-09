b70dbc4ae1d0210e44bb1efaeb6f7fbba9b3c894
hayato6911 <hayato6911@yahoo.co.jp>
2021-10-10 00:18:37 +0900

    map機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_174720) do
+ActiveRecord::Schema.define(version: 2021_10_09_113202) do
 
   create_table "locations", force: :cascade do |t|
     t.string "location_name"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_174720) do
     t.string "user_id"
     t.string "introduction"
     t.float "latitude"
-    t.string "longitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
