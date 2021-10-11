2a86eeb757fd5670f45345bab83338b47da7782f
hayato6911 <hayato6911@yahoo.co.jp>
2021-10-11 19:22:27 +0900

    星の平均を出す

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,23 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def index
+    @location = Location.find(params[:location_id])
+    @reviews = @location.reviews
+  end
+  
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to location_reviews_path(@review.location)
+    else
+      @location = Location.find(params[:location_id])
+      render "locations/show"
+    end
+  end
+  
+  private
+  def review_params
+    params.require(:review).permit(:location_id, :score, :content)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :location
+  validates :score, presence: true
 end
