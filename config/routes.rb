5f8146d2aaa534267d944ceb8efff97522afe0c0
hayato6911 <hayato6911@yahoo.co.jp>
2021-11-01 07:43:59 +0900

    ユーザー編集の際にパスワード不要

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'locations/posts'
   get 'helpful/index'
   resources :locations
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
   root to: "home#index"
   
   resources :users, :only => [:posts, :index, :show]
