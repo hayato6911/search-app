cc4f71e11eb4ba5f44e9495a21a076d5ff40a4a2
hayato6911 <hayato6911@yahoo.co.jp>
2021-11-02 10:55:15 +0900

    重複文消去

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-        validates :username, presence: true
   
   has_many :locations, dependent: :destroy
   has_many :reviews, dependent: :destroy
